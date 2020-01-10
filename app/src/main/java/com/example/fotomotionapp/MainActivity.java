ce2a60529a7431285baea679b47ecb21683d807a
Mariscos1 <ramon.marquez3@yahoo.com>
2020-01-10 15:48:09 -0600

    hey hey - Ramon

--- app/src/main/java/com/example/fotomotionapp/MainActivity.java
+++ app/src/main/java/com/example/fotomotionapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Big Boy make me happy
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
