b3fddd4891e6eeae7cf224f33b74551ab7f361cd
jay-acosta <jayacosta@utexas.edu>
2020-01-12 20:32:49 -0600

    added something to tell user the truth when pressing the button

--- app/src/main/java/com/example/fotomotionapp/MainActivity.java
+++ app/src/main/java/com/example/fotomotionapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.fotomotionapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get the button by finding the elements with the id of "straightButton"
+        Button straightButton = findViewById(R.id.straightButton);
+
+        // set the action to be performed on click to an iClicker
+        straightButton.setOnClickListener(new iClicker());
+
+
+    }
+
+    private class iClicker implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+            // notifies user about what the button really does
+            Toast.makeText(MainActivity.this,"you still non-hetero lmao", Toast.LENGTH_SHORT).show();
+        }
     }
 }
