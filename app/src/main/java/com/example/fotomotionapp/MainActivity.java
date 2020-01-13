02d0ea0031e3cb34b1db351aa09587bcf0bffc5b
Janlloyd Carangan <malphene@gmail.com>
2020-01-13 14:39:45 -0600

    dynamic button creator

--- app/src/main/java/com/example/fotomotionapp/MainActivity.java
+++ app/src/main/java/com/example/fotomotionapp/MainActivity.java
@@ -1,20 +1,26 @@
 package com.example.fotomotionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout myLayout;
 
     //Big Boy make me happy
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myLayout = findViewById(R.id.layout);
 
         final Button switchScreenButton = (Button) findViewById(R.id.switchScreenButton);
         switchScreenButton.setOnClickListener(new View.OnClickListener() {
@@ -23,6 +29,22 @@ public class MainActivity extends AppCompatActivity {
                 Intent switchScreenIntent = new Intent(getApplicationContext(), PaintActivity.class);
                 switchScreenIntent.putExtra("com.example.fotomotionapp.buttonText", switchScreenButton.getText().toString());
                 startActivity(switchScreenIntent);
+            }
+        });
+
+        Button addPanelsButton = findViewById(R.id.addPanelsButton);
+        addPanelsButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Button additionalButton = new Button(MainActivity.this);
+
+                additionalButton.setLayoutParams(new ConstraintLayout.LayoutParams(
+                        ConstraintLayout.LayoutParams.MATCH_CONSTRAINT,
+                        ConstraintLayout.LayoutParams.WRAP_CONTENT
+                ));
+
+                myLayout.addView(additionalButton);
             }
         });
 
