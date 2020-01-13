511b5a5a5c367c0e828ba4fddaeb7d6d823e9fa4
Janlloyd Carangan <malphene@gmail.com>
2020-01-13 14:06:07 -0600

    added second screen plus some intent examples

--- app/src/main/java/com/example/fotomotionapp/MainActivity.java
+++ app/src/main/java/com/example/fotomotionapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.fotomotionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button switchScreenButton = (Button) findViewById(R.id.switchScreenButton);
+        switchScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent switchScreenIntent = new Intent(getApplicationContext(), PaintActivity.class);
+                startActivity(switchScreenIntent);
+            }
+        });
 
         // get the button by finding the elements with the id of "straightButton"
         Button straightButton = findViewById(R.id.fileButton);
@@ -28,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View v) {
+
 
             // notifies user about what the button really does
             Toast.makeText(MainActivity.this,"you still non-hetero lmao", Toast.LENGTH_SHORT).show();
