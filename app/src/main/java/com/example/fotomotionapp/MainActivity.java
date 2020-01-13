3494cb27f381fcbf2e8cfa9143c768d4af027dc3
Mariscos1 <ramon.marquez3@yahoo.com>
2020-01-13 15:16:33 -0600

    It can now make a new folder to put all of your projects in I guess - Ramon

--- app/src/main/java/com/example/fotomotionapp/MainActivity.java
+++ app/src/main/java/com/example/fotomotionapp/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.fotomotionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
 import android.widget.Toast;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String directory = "/FOTOMOTION_APP";
+    TableLayout myLayout;
+    int counter;
 
     //Big Boy make me happy
     @Override
@@ -16,13 +25,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button switchScreenButton = (Button) findViewById(R.id.switchScreenButton);
+        myLayout = findViewById(R.id.layout);
+
+        final Button switchScreenButton = findViewById(R.id.switchScreenButton);
         switchScreenButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent switchScreenIntent = new Intent(getApplicationContext(), PaintActivity.class);
                 switchScreenIntent.putExtra("com.example.fotomotionapp.buttonText", switchScreenButton.getText().toString());
                 startActivity(switchScreenIntent);
+            }
+        });
+
+        Button addPanelsButton = findViewById(R.id.addPanelsButton);
+        addPanelsButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Button additionalButton = new Button(MainActivity.this);
+                additionalButton.setText("Project: " + counter++);
+
+                additionalButton.setLayoutParams(new TableLayout.LayoutParams(
+                        TableLayout.LayoutParams.WRAP_CONTENT,
+                        TableLayout.LayoutParams.WRAP_CONTENT
+
+                ));
+
+                myLayout.addView(additionalButton);
             }
         });
 
@@ -43,6 +72,14 @@ public class MainActivity extends AppCompatActivity {
 
             // notifies user about what the button really does
             Toast.makeText(MainActivity.this,"you still non-hetero lmao", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void makeDirectory(){
+        File myDir = new File(Environment.getExternalStorageDirectory() + directory);
+        if(!myDir.exists()){
+            myDir.mkdir();
+            Toast.makeText(MainActivity.this, "Directory has been made", Toast.LENGTH_SHORT).show();
         }
     }
 }
