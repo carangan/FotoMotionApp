ba4a6d02ae16ca3bcc2e74a5288a0b78ce4602a0
Mariscos1 <ramon.marquez3@yahoo.com>
2020-01-13 15:04:34 -0600

    Merge branch 'master' of /Users/ramonmarquez/AndroidStudioProjects/FotoMotionApp with conflicts.

--- app/src/main/java/com/example/fotomotionapp/MainActivity.java
+++ app/src/main/java/com/example/fotomotionapp/MainActivity.java
@@ -1,19 +1,14 @@
 package com.example.fotomotionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TableLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    TableLayout myLayout;
-    int counter;
 
     //Big Boy make me happy
     @Override
@@ -21,33 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myLayout = findViewById(R.id.layout);
-
-        final Button switchScreenButton = findViewById(R.id.switchScreenButton);
+        final Button switchScreenButton = (Button) findViewById(R.id.switchScreenButton);
         switchScreenButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent switchScreenIntent = new Intent(getApplicationContext(), PaintActivity.class);
                 switchScreenIntent.putExtra("com.example.fotomotionapp.buttonText", switchScreenButton.getText().toString());
                 startActivity(switchScreenIntent);
-            }
-        });
-
-        Button addPanelsButton = findViewById(R.id.addPanelsButton);
-        addPanelsButton.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                Button additionalButton = new Button(MainActivity.this);
-                additionalButton.setText("Project: " + counter++);
-
-                additionalButton.setLayoutParams(new TableLayout.LayoutParams(
-                        TableLayout.LayoutParams.WRAP_CONTENT,
-                        TableLayout.LayoutParams.WRAP_CONTENT
-
-                ));
-
-                myLayout.addView(additionalButton);
             }
         });
 
